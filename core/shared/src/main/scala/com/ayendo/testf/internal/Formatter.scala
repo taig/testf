840b297aecdc450a025a842c7dee0c8dd5627b27
Niklas Klein <mail@taig.io>
2019-01-27 00:28:50 +0100

    Restore entire summary test suite

--- core/shared/src/main/scala/com/ayendo/testf/internal/Formatter.scala
+++ core/shared/src/main/scala/com/ayendo/testf/internal/Formatter.scala
@@ -21,22 +21,28 @@ object Formatter {
   }
 
   def summary(summary: Summary, color: Boolean): String =
-    this.summary(color, expanded = summary.isErrorOrFailure, level = 0)(summary)
+    this.summary(color, level = 0)(summary)
 
-  def summary(color: Boolean,
-              expanded: Boolean,
-              level: Int): Summary => String = {
+  def summary(color: Boolean, level: Int): Summary => String = {
     case Summary.Error(description, message) =>
       error(description, Some(message), color)
     case Summary.Failure(description, throwable) =>
       failure(description, throwable, color)
-    case Summary.Success(description) => success(description, color)
     case Summary.Group(summaries, None) if level === 0 =>
-      summaries.map(summary(color, expanded, level + 1)).mkString(EOL)
+      summaries.map(summary(color, level + 1)).mkString(EOL)
     case Summary.Group(_, Some(description)) if level === 0 =>
       success(description, color)
-    case Summary.Group(_, Some(description)) =>
-      success(description, color)
+    case group @ Summary.Group(summaries, description) =>
+      val title = description.getOrElse(Formatter.description(group))
+
+      if (group.isSuccess) success(title, color)
+      else {
+        val message = error(title, message = None, color)
+        val details = summaries.map(summary(color, level + 1)).mkString(EOL)
+        message + EOL + Text.padLeft(details, 2)
+      }
+    case Summary.Skip(description)    => skip(description, color)
+    case Summary.Success(description) => success(description, color)
   }
 
   def error(description: String,
@@ -54,6 +60,11 @@ object Formatter {
     val value = s"⚡$description" + EOL + Text.padLeft(error, 2)
 
     if (color) Text.colorize(value, Console.RED) else value
+  }
+
+  def skip(description: String, color: Boolean): String = {
+    val value = s"~ $description"
+    if (color) Text.colorize(value, Console.YELLOW) else value
   }
 
   def success(description: String, color: Boolean): String = {
--- core/shared/src/test/scala/com/ayendo/testf/SummaryTest.scala
+++ core/shared/src/test/scala/com/ayendo/testf/SummaryTest.scala
@@ -23,6 +23,9 @@ object SummaryTest extends TestF {
                                                                 |$details""".stripMargin)
   }
 
+  val showSkip: Assert[Id] =
+    showSummary(Summary.Skip("skip")).equal("~ skip")
+
   val showSuccess: Assert[Id] =
     showSummary(Summary.Success("success")).equal("✓ success")
 
@@ -55,94 +58,124 @@ object SummaryTest extends TestF {
                                |✓ s1""".stripMargin)
   }
 
-//  val showGroupWithDescriptionSuccess: Assert = {
-//    val group: Summary = Summary.Group(
-//      Summary.Success("s1"),
-//      Summary.Success("s2"),
-//      description = Some("group")
-//    )
-//
-//    showSummary(group).equal("✓ group")
-//  }
-//
-//  val showGroupWithoutDescriptionError: Assert = {
-//    val group: Summary = Summary.Group(
-//      Summary.Success("success"),
-//      Summary.Error("error", "reason"),
-//      description = None
-//    )
-//
-//    showSummary(group).equal("""✗ success |+| error
-//                  |  ✓ success
-//                  |  ✗ error
-//                  |    reason""".stripMargin)
-//  }
-//
-//  val showGroupWithDescriptionError: Assert = {
-//    val group: Summary = Summary.Group(
-//      Summary.Success("success"),
-//      Summary.Error("error", "reason"),
-//      description = Some("group")
-//    )
-//
-//    showSummary(group).equal("""✗ group
-//                  |  ✓ success
-//                  |  ✗ error
-//                  |    reason""".stripMargin)
-//  }
-//
-//  val showNestedGroupWithoutDescriptionError: Assert = {
-//    val group: Summary = Summary.Group(
-//      Summary.Group(Summary.Success("s1"),
-//                    Summary.Success("s2"),
-//                    description = None),
-//      Summary.Error("error", "reason"),
-//      description = None
-//    )
-//
-//    showSummary(group).equal(
-//      """✗ s1 |+| s2 |+| error
-//        |  ✓ s1
-//        |  ✓ s2
-//        |  ✗ error
-//        |    reason""".stripMargin
-//    )
-//  }
-//
-//  val showNestedGroupWithDescriptionError: Assert = {
-//    val group: Summary = Summary.Group(
-//      Summary.Group(Summary.Success("s1"),
-//                    Summary.Success("s2"),
-//                    description = Some("g1")),
-//      Summary.Error("error", "reason"),
-//      description = None
-//    )
-//
-//    showSummary(group).equal("""✗ g1 |+| error
-//                  |  ✓ g1
-//                  |  ✗ error
-//                  |    reason""".stripMargin)
-//  }
+  val showGroupNestedWithoutDescriptionSuccess: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(List(Summary.Success("s1"), Summary.Success("s2")),
+                      description = None)),
+      description = None)
 
-//  override val suite: List[Assert] = List(
-//    showError,
-//    showFailure,
-//    showSuccess,
-//    showGroupWithoutDescriptionSuccess,
-//    showGroupWithRepeatedDescriptionsSuccess,
-//    showGroupWithDescriptionSuccess,
-//    showGroupWithoutDescriptionError,
-//    showGroupWithDescriptionError,
-//    showNestedGroupWithoutDescriptionError,
-//    showNestedGroupWithDescriptionError
-//  )
+    showSummary(group).equal("✓ s1 |+| s2")
+  }
+
+  val showGroupNestedWithRepeatedDescriptionSuccess: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(List(Summary.Success("s1"), Summary.Success("s1")),
+                      description = None)),
+      description = None)
+
+    showSummary(group).equal("✓ s1")
+  }
+
+  val showGroupNestedWithDescriptionSuccess: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(List(Summary.Success("s1"), Summary.Success("s2")),
+                      description = Some("group"))),
+      description = None)
+
+    showSummary(group).equal("✓ group")
+  }
+
+  val showGroupNestedWithoutDescriptionError: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(List(Summary.Success("success"),
+                           Summary.Error("error", "reason")),
+                      description = None)),
+      description = None
+    )
+
+    showSummary(group).equal("""✗ success |+| error
+                               |  ✓ success
+                               |  ✗ error
+                               |    reason""".stripMargin)
+  }
+
+  val showGroupNestedWithDescriptionError: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(List(Summary.Success("success"),
+                           Summary.Error("error", "reason")),
+                      description = Some("group"))),
+      description = None)
+
+    showSummary(group).equal("""✗ group
+                  |  ✓ success
+                  |  ✗ error
+                  |    reason""".stripMargin)
+  }
+
+  val showGroupDeeplyNestedWithoutDescriptionError: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(
+          List(
+            Summary.Group(List(Summary.Success("s1"), Summary.Success("s2")),
+                          description = None),
+            Summary.Error("error", "reason"),
+          ),
+          description = None
+        )),
+      description = None
+    )
+
+    showSummary(group).equal(
+      """✗ s1 |+| s2 |+| error
+        |  ✓ s1 |+| s2
+        |  ✗ error
+        |    reason""".stripMargin
+    )
+  }
+
+  val showGroupDeeplyNestedWithDescriptionError: Assert[Id] = {
+    val group: Summary = Summary.Group(
+      List(
+        Summary.Group(
+          List(
+            Summary.Group(List(Summary.Success("s1"), Summary.Success("s2")),
+                          description = Some("g1")),
+            Summary.Error("error", "reason"),
+          ),
+          description = None
+        )),
+      description = None
+    )
+
+    showSummary(group).equal(
+      """✗ g1 |+| error
+        |  ✓ g1
+        |  ✗ error
+        |    reason""".stripMargin
+    )
+  }
 
   override val suite: Assert[IO] =
     (
       showError |+|
         showFailure |+|
+        showSkip |+|
+        showSuccess |+|
         showGroupRootWithoutDescriptionSuccess |+|
         showGroupRootWithDescriptionSuccess |+|
-        showGroupRootWithRepeatedDescriptionsSuccess
+        showGroupRootWithRepeatedDescriptionsSuccess |+|
+        showGroupNestedWithDescriptionSuccess |+|
+        showGroupNestedWithRepeatedDescriptionSuccess |+|
+        showGroupNestedWithoutDescriptionSuccess |+|
+        showGroupNestedWithoutDescriptionError |+|
+        showGroupNestedWithDescriptionError |+|
+        showGroupDeeplyNestedWithoutDescriptionError |+|
+        showGroupDeeplyNestedWithDescriptionError
     ).liftIO
 }
