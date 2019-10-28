f0d2b8d095c1db943a28da6a9223149408411ff6
Niklas Klein <mail@taig.io>
2019-10-28 09:49:03 +0100

    Add more interpreter instances

--- core/src/main/scala/io/taig/testf/Interpreter.scala
+++ core/src/main/scala/io/taig/testf/Interpreter.scala
@@ -1,27 +1,27 @@
 package io.taig.testf
 
 import cats._
-import cats.effect.IO
+import cats.effect.{Effect, IO}
+import cats.effect.implicits._
 import cats.implicits._
 
 trait Interpreter[F[_], G[_]] {
   def interpret[A](test: Test[F, A]): G[Test[Pure, A]]
 }
 
-object Interpreter extends Interpreter1 {
-  implicit def effect[F[_]](
-      implicit F: MonadError[F, Throwable],
-      P: Parallel[F]
-  ): Interpreter[F, F] =
-    new Interpreter[F, F] {
-      override def interpret[A](test: Test[F, A]): F[Test[Pure, A]] =
+object Interpreter extends Interpreter1 with Interpreter2 {
+  implicit def effectIOParallel[F[_]: Effect](
+      implicit P: Parallel[IO]
+  ): Interpreter[F, IO] =
+    new Interpreter[F, IO] {
+      override def interpret[A](test: Test[F, A]): IO[Test[Pure, A]] =
         test match {
           case test: Test.And[F, A] =>
             test.tests.parTraverse(interpret[A]).map(Test.and)
           case test: Test.Eval[F, A] =>
-            test.test.flatMap(interpret[A]).handleError(Test.failure)
-          case test: Test.Error   => test.pure[F].widen
-          case test: Test.Failure => test.pure[F].widen
+            test.test.toIO.flatMap(interpret[A]).handleError(Test.failure)
+          case test: Test.Error   => test.pure[IO].widen
+          case test: Test.Failure => test.pure[IO].widen
           case test: Test.Label[F, A] =>
             interpret(test.test).map(Test.Label(test.description, _))
           case test: Test.Message[F, A] =>
@@ -30,7 +30,7 @@ object Interpreter extends Interpreter1 {
           case test: Test.Or[F, A] =>
             test.tests.parTraverse(interpret[A]).map(Test.or)
           case test: Test.Skip[F, A] => interpret(test.test).map(Test.skip)
-          case test: Test.Success[A] => test.pure[F].widen
+          case test: Test.Success[A] => test.pure[IO].widen
         }
     }
 
@@ -66,6 +66,30 @@ trait Interpreter1 {
             test.tests.traverse(interpret[A]).map(Test.or)
           case test: Test.Skip[F, A] => interpret(test.test).map(Test.skip)
           case test: Test.Success[A] => test.pure[F].widen
+        }
+    }
+}
+
+trait Interpreter2 {
+  implicit def effectIOSequential[F[_]: Effect]: Interpreter[F, IO] =
+    new Interpreter[F, IO] {
+      override def interpret[A](test: Test[F, A]): IO[Test[Pure, A]] =
+        test match {
+          case test: Test.And[F, A] =>
+            test.tests.traverse(interpret[A]).map(Test.and)
+          case test: Test.Eval[F, A] =>
+            test.test.toIO.flatMap(interpret[A]).handleError(Test.failure)
+          case test: Test.Error   => test.pure[IO].widen
+          case test: Test.Failure => test.pure[IO].widen
+          case test: Test.Label[F, A] =>
+            interpret(test.test).map(Test.Label(test.description, _))
+          case test: Test.Message[F, A] =>
+            interpret(test.test).map(Test.Message(test.description, _))
+          case test: Test.Not[F, A] => interpret(test.test).map(Test.not)
+          case test: Test.Or[F, A] =>
+            test.tests.traverse(interpret[A]).map(Test.or)
+          case test: Test.Skip[F, A] => interpret(test.test).map(Test.skip)
+          case test: Test.Success[A] => test.pure[IO].widen
         }
     }
 }
