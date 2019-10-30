18d94f2ebe4343e0bf47468ea415e8bce55e5c4e
Niklas Klein <mail@taig.io>
2019-10-30 11:11:57 +0100

    Add Timer[IO] instance to IOTestApp

--- core/src/main/scala/io/taig/testf/internal/Contexts.scala
+++ core/src/main/scala/io/taig/testf/internal/Contexts.scala
@@ -1,12 +1,14 @@
 package io.taig.testf.internal
 
-import cats.effect.{Blocker, ContextShift, IO}
+import cats.effect.{Blocker, ContextShift, IO, Timer}
 
 import scala.concurrent.ExecutionContext
 
 object Contexts {
+  val blocker: Blocker = Blocker.liftExecutionContext(ExecutionContext.global)
+
   val contextShift: ContextShift[IO] =
     IO.contextShift(ExecutionContext.global)
 
-  val blocker: Blocker = Blocker.liftExecutionContext(ExecutionContext.global)
+  val timer: Timer[IO] = IO.timer(ExecutionContext.global)
 }
